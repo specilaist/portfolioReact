7bc9ce92dad1db12554cfd2e3b25c74ce33789b7
specilaist <specilaist@msn.com>
2021-03-21 21:44:56 -0700

    deleted react filler, began working on cards for contact page and project page, working on inputing images into cards

--- client/src/component/aboutMe.js
+++ client/src/component/aboutMe.js
@@ -1,35 +1,63 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
+import CardActions from "@material-ui/core/CardActions";
+import Collapse from "@material-ui/core/Collapse";
+import RecipeReviewCard from './projectCard/cardExample'
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    '& > *': {
-      margin: theme.spacing(1),
+    display: "flex",
+    flexWrap: "wrap",
+    "& > *": {
+      margin: theme.spacing(2),
       width: theme.spacing(100),
-      height: theme.spacing(100),
     },
+  },
+  paper: {},
+  text: {
+    margin: theme.spacing(2),
   },
 }));
 
 const AboutMe = () => {
-      const classes = useStyles();
+  const classes = useStyles();
 
-      return (
-            <div className={classes.root}>
-                  <Paper elevation={5}>
-                        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus at imperdiet quam. Suspendisse maximus ornare convallis. Quisque feugiat nisi ut felis feugiat tempus. Curabitur sed mi elementum mi laoreet euismod in eget augue. Sed posuere augue sit amet purus pellentesque, ut sodales mauris cursus. Nunc tempor sem convallis mi laoreet, nec tempor mauris luctus. Sed purus justo, iaculis ut bibendum non, auctor sed velit. Nam aliquam urna in felis sollicitudin, ac posuere dui fermentum. Donec tristique nec erat vitae ornare. Suspendisse nec lorem ac est ullamcorper lobortis et id sem. Sed porttitor tortor non eros auctor facilisis. Morbi eleifend mi justo, eget efficitur dolor iaculis in. Suspendisse mollis posuere porta. </p>
+  return (
+    <div className={classes.root}>
+			<Paper>
+				<h1>My Contact Info</h1>
 
-<p>Integer felis eros, varius eget iaculis finibus, elementum vitae velit. Curabitur faucibus efficitur mi quis porttitor. Pellentesque suscipit velit eros, sit amet cursus mi pulvinar eu. Vivamus ut enim vitae sapien dictum suscipit id nec mi. Donec fermentum est ligula, vel placerat quam finibus non. Duis placerat at felis sit amet lobortis. Quisque accumsan tempus sem, et bibendum ligula porttitor ac. Fusce at efficitur ante. Vivamus aliquet turpis libero, vitae varius neque condimentum vel. Cras eget pellentesque ex. Aliquam eu tempus est.</p>
-
-<p>Ut porttitor tristique quam, non aliquam lectus pharetra non. Donec in ex ex. Quisque dignissim leo vel diam dictum, id maximus sem ultricies. Vestibulum malesuada volutpat orci vel congue. Nunc condimentum, quam sit amet dapibus pharetra, elit nisi rhoncus justo, et egestas nibh nibh eu urna. Mauris pellentesque magna quis dolor aliquet, eget tempor lorem lobortis. Sed cursus ac lorem et convallis. Suspendisse sed dui iaculis, molestie nisl id, bibendum nibh. Duis varius porttitor erat ac congue. Praesent leo massa, pretium a scelerisque a, vehicula lacinia turpis. Suspendisse posuere risus sit amet neque lobortis tristique. Donec tortor risus, viverra non ipsum ac, aliquet pulvinar sem. Integer in convallis ex, ac rhoncus justo.</p>
-                  </Paper>
-
-            </div>
-      )
-
-}
+			</Paper>
+			<Card>
+				<h2>About Me</h2>
+				<img alt='Eban' src='../294E9632-E211-4254-ABAA-40E1D1B172FC_1_105_c.jpeg'/>
+          <CardHeader>
+						<Typography component="h2">
+							About Me
+						</Typography>
+          </CardHeader>
+          <CardMedia image="../294E9632-E211-4254-ABAA-40E1D1B172FC_1_105_c.jpeg" ></CardMedia>
+          <CardContent>
+						<p>Hello,</p>
+            My name is Eban Cambridge and seeking a position as a Full Stack Web
+            Developer. I am a person who loves to learn new things. It started
+            when I worked towards becoming an Eagle Scout with the Boy Scouts of
+            America. I learned through the process of getting merit badges the
+            benefits of a diverse learning background, and how well rounded it
+            can make a person. I like web development for the same concept of
+            continual learning and applying new skills to an ever-evolving
+            implementation.
+						<p>As a professional I've learned the joy of helping businesses realize their objectives. I think Web Development is a great way to help businesses streamline their operations and make data work for them. I collaborated with many companies as a Senior Executive with the Boy Scouts of America. I also worked within a team of fellow associates and volunteers to meet organizational goals. As a Business Assistant Consultant contracted to assist businesses in Oakland with infrastructure redevelopment, I know how to listen and provide solutions. In every profession I have experienced the difficulties of working with clunky software and am looking to be the change I seek in the world. I look forward to being able to make a more streamlined, user friendly, product for whomever I am helping.</p>
+          </CardContent>
+        </Card>
+    </div>
+  );
+};
 
 export default AboutMe;
